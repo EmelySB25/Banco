4c603ee49f66b91be70d9f898c1bb171421850e1
emely <emelysb404@gmail.com>
2021-04-18 18:37:32 -0500

    first commit

---
+++ Banco/Models/Ejemplo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Banco.Models
+{
+    public class Ejemplo
+    {
+        public int ID_Loa { get; set; }
+        public string Nombre { get; set; }
+    }
+}
